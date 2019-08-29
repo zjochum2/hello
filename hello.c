ca2e9cfdde0b12b54e8f0813c3026c06eb04c07c
zjochum2 <jochumscott@gmail.com>
2019-08-29 11:15:14 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Zach Jochum
+ * Date: 08/28/2019
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Zach Jochum\n");
+
+  return 0;
+}
